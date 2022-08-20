0200142c6ebf9890171e65a15e9168413ad8e54e
abdimalikomar@dev.azure.com <malik@maliks-MacBook-Air.local>
2022-08-20 20:18:56 +0200

    files added

--- terraform-manifest/variables.tf
+++ terraform-manifest/variables.tf
@@ -21,7 +21,7 @@ variable "resource_group_name" {
 variable "env" {
   type        = string
   description = "This variable defines the Environment"
-  default     = "dev"
+  #default     = "dev"
 }
 
 
